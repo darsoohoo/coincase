ba9194732af0eb282f9049ef1d0988d9bb810af9
Darren SooHoo <darsoohoo@gmail.com>
2018-12-05 21:38:21 -0800

    added binance-data.js

---
+++ binance-data.js
@@ -0,0 +1,31 @@
+
+/*
+  var burl = "https://api.binance.com";
+  var query = '/api/v3/ticker/price'
+  //query += '?symbol=BTCUSDT';
+  var url = burl + query;
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET',url,true);
+  ourRequest.onload = function(){
+    console.log(ourRequest.responseText);
+  }
+  ourRequest.send();
+
+*/
+
+
+
+  fetch('https://api.binance.com/api/v3/ticker/price')
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+
+
+    let coinArray = data.array;
+    let btcPrice = coinArray;
+    console.log(data);
+
+  }).catch(err => {
+    // Do something for an error here
+  });
